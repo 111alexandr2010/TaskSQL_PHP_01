810a3398f797e38604d711c617823dc3ade2821f
111alexandr2010 <43431190+111alexandr2010@users.noreply.github.com>
2018-12-13 13:57:04 +0700

    Add files via upload

---
+++ TaskSQL_PHP_09.php
@@ -0,0 +1,105 @@
+<?php
+
+$dsn = 'mysql:dbname=clinic;host=127.0.0.1:3307';
+$user = 'root';
+$password = '';
+
+try {
+    $dbh = new PDO($dsn, $user, $password);
+
+    $sql = 'SELECT * FROM kindAnimals';
+
+    $sth = $dbh->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+    $sth->execute(array());
+
+    if (isset ($_GET['Delete'])) {
+        $rowNumber = (int)$_GET['Delete'];
+        rowDelete($dbh, $rowNumber);
+        header('Connection:/SQL_PHP/TaskSQL_PHP_09.php');
+    }
+
+    if (isset ($_GET['nameInRussian']) && isset ($_GET['nameInLatin'])) {
+        $nameInRussian = $_GET['nameInRussian'];
+        $nameInLatin = $_GET['nameInLatin'];
+
+        if (isset ($_GET['insert']) && $nameInRussian != null && $nameInLatin != null) {
+            rowInsert($dbh, $nameInRussian, $nameInLatin);
+            header('Connection:/SQL_PHP/TaskSQL_PHP_09.php');
+        }
+    }
+
+    createTable($sth->fetchAll());
+
+} catch (PDOException $e) {
+    echo 'Соединение не установлено!' . $e->getMessage();
+}
+
+function rowInsert($dbh, string $nameInRussian, string $nameInLatin)
+{
+    $dbh->exec("INSERT INTO kindAnimals(nameInRussian, nameInLatin)
+                VALUES ('$nameInRussian', '$nameInLatin')");
+}
+
+function rowDelete($dbh, int $rowNumber)
+{
+    $dbh->exec("DELETE FROM kindAnimals WHERE id = $rowNumber");
+}
+
+function createTable($array)
+{
+    ?>
+    <html>
+    <body>
+    <form method="get" action="/SQL_PHP/TaskSQL_PHP_09.php">
+        <table width="70%" border="1">
+            <thead>
+            <style type="text/css">
+                TH {
+                    background: yellow;
+                    color: black;
+                }
+
+                TD {
+                    background: whitesmoke;
+                    color: grey;
+                }
+            </style>
+            <tr>
+                <th>___id___</th>
+                <th>nameInRussian</th>
+                <th>nameInLatin</th>
+                <th>isDelete / isInsert</th>
+            </tr>
+            </thead>
+            <?php
+            for ($i = 0; $i < count($array); $i++) {
+                echo '<tr>';
+                for ($j = 0; $j < 3; $j++) {
+                    ?>
+                    <td><?= $array[$i][$j] ?></td>
+                <?php } ?>
+                <td>
+                <form method="get" action="/SQL_PHP/TaskSQL_PHP_09.php">
+                    <p style="color: red"><input type="submit" name="Delete"
+                                                 value="<?= $array[$i][0] ?>. 'Delete'"></p>
+                </form></td><?php
+                echo '</tr>';
+            }
+            ?>
+            <form method="get" action="/SQL_PHP/TaskSQL_PHP_09.php">
+                <td></td>
+                <td><label for="nameInRussian">Введите nameInRussian </label>
+                    <input type="text" id="nameInRussian" name="nameInRussian"></td>
+                <td><label for="nameInLatin">Введите nameInLatin </label>
+                    <input type="text" id="nameInLatin" name="nameInLatin"></td>
+                <td>
+                    <p><input type="submit" name="insert" value="Добавить строку"></p>
+            </form>
+            </td>
+        </table>
+    </form>
+    </body>
+    </html>
+    <?php
+}
+?>
